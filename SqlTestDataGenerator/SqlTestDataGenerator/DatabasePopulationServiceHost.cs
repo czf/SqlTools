8c17129d79a7a598f65823fb16d744fc55d2d383
czf <2455501+czf@users.noreply.github.com>
2021-08-15 14:37:01 -0700

    initial setup of console app

---
+++ SqlTestDataGenerator/SqlTestDataGenerator/DatabasePopulationServiceHost.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace SqlTestDataGenerator
+{
+    public class DatabasePopulationServiceHost : BackgroundService
+    {
+        private readonly IHostApplicationLifetime _applicationLifetime;
+
+        public DatabasePopulationServiceHost(IHostApplicationLifetime applicationLifetime)
+        {
+            _applicationLifetime = applicationLifetime;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+
+            }
+            finally
+            {
+
+                _applicationLifetime.StopApplication();
+            } 
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SqlTestDataGenerator/SqlTestDataGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SqlTestDataGenerator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    services.Configure<HostServiceConfig>(hostContext.Configuration);
+                    services.AddHostedService<DatabasePopulationServiceHost>();
+                })
+                .Build();
+
+
+            await host.RunAsync();
+
+        }
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args);
+
+    }
+    public class HostServiceConfig { }
+
+}
